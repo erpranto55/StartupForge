035736aac96acc94613da5c5feaefd60e641cf4d
erpranto <erpranto55@gmail.com>
2026-06-30 02:27:43 +0600

    Axios Updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
+const getCookie = (name) => {
+  if (typeof document === "undefined") return null;
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+  return null;
+};
+
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, ""),
   withCredentials: true,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = getCookie("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default instance;
